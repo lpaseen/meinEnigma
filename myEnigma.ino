9142b56925672037a21025ce6d8c37a83b3697fa
lpaseen <peters-github@techwiz.ca>
2015-12-04 00:39:47 -0500

    Started to add wheel code

--- myEnigma.ino
+++ myEnigma.ino
@@ -1,7 +1,8 @@
 /****************************************************************
    MyEnigma
    v0.00 - test of library, keyboard and LEDs
-   v0.01 - more parts now working, just missing rotor and enigma code.
+   v0.01 - more parts now working, just missing wheel and enigma code.
+   v0.02 - added wheel code
 */
 
 #include "ht16k33.h"
@@ -9,6 +10,23 @@
 #include <EEPROM.h>
 #include <util/crc16.h>
 #include <Wire.h>
+
+//Define the input pin for each of the 4 wheel encoders
+//[wheel] [low, high]
+//[wheel1low,wheel1high,wheel2low,wheel2hig...
+
+#define WALZECNT 4
+//if count is something else than 4 the pins this definitions also need to change
+volatile uint8_t encoderPins[WALZECNT * 2] = {2, 3, 4, 5, 6, 7, 8, 9};
+volatile uint8_t encoderState[WALZECNT] = {0xff, 0xff, 0xff, 0xff};
+volatile boolean encoderMoved[WALZECNT] = {false, false, false, false};
+//volatile uint8_t walze[WALZECNT] = {0, 0, 0, 0};
+
+const char walzeContent[28] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+typedef struct {
+  char letter[26];         // 26 to allow external Uhr box connected, then A->B doesn't mean B->A.
+} letters_t;
 
 //enigma models
 // http://www.cryptomuseum.com/crypto/enigma/timeline.htm
@@ -24,27 +42,29 @@
 
 typedef enum {M3,M4} enigmaModel_t;
 
-typedef struct {
-  char letter[26];         // 26 to allow external Uhr box connected, then A->B doesn't mean B->A.
-} letters_t;
-
 //TODO: 
 // Add something so we can have several presets in memory and then change between them
 
 // EEPROM structure version - to be able to handle upgrades
 #define VERSION 0
 
+// rotor/wheel = walze
+// Entry wheel = Eintrittswalze (EKW), static
+// Reflector   = Umkehrwalze (UKW)
+// wheels are counted from right to left so
+//  rightmost wheel is 0
+//  leftmost wheel is 3 and not available on M3
+
 typedef struct {
   uint8_t fwVersion;         // firmware version
   uint8_t preset;            // Allow multiple saved settings
   enigmaModel_t model;
   uint8_t ukw;               // what reflector that is loaded
-  uint8_t rotor[4];          // current wheel no in the 4 or 3 positions
-  char ringstellung[4];      // Setting of the 4 rotors ring.
-  //char plugboard[26];        // 26 to allow external Uhr box connected, then A->B doesn't mean B->A.
+  uint8_t walze[WALZECNT];          // what wheel that currently is in the 3 or 4 positions
+  char ringstellung[WALZECNT];      // Setting of the wheel ring.
   letters_t plugboard;
   unsigned long odometer;    // How many characters this unit has en/decrypted
-  uint8_t currentrotor[4];   // current position of the 4 rotors
+  char currentWalze[WALZECNT];   // current position of the wheel
   unsigned int nextlocation; // start for next section in eeprom
   boolean valid;             // whatever this section is valid or not
   unsigned int checksum;
@@ -81,7 +101,6 @@ HT16K33::KEYDATA keys;
 #define GPIOB     0x13 // 
 #define OLATA     0x14 // Output latch register
 #define OLATB     0x15 // 
-
 
 
 
@@ -243,6 +262,44 @@ uint8_t readSettings() {
   return 1; // csum invalid so no valid data
 } // readSettings
 
+//Some code from http://playground.arduino.cc/Main/PinChangeInterrupt
+//This is tested on an arduino uno and arduino nano, other models may not work
+void pciSetup(uint8_t pin)
+{
+  *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+  PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+  PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+//BUG: not much to debounce code!
+// and that seems to be needed, how to do that with interrupts?
+
+void updateEncoderState() {
+  uint8_t i, state, state2;
+
+  //read state of all 4 encoders
+  for (i = 0; i < sizeof(encoderPins); i += 2) {
+    state = 0; // clear from prev round
+    state = digitalRead(encoderPins[i + 1]) << 1 | digitalRead(encoderPins[i]);
+    delay(10); //debounce
+    state2 = digitalRead(encoderPins[i + 1]) << 1 | digitalRead(encoderPins[i]);
+    if (state == state2) {
+      if ((encoderState[i / 2] & 3) != state) { // same as before ?
+        encoderState[i / 2] = encoderState[i / 2] << 2 | state;
+        encoderMoved[i / 2] = true;
+      } // if state changed
+    } // if no bounce
+  } // for
+} //updateEncoderState
+
+ISR (PCINT2_vect) { // handle pin change interrupt for D0 to D7 here
+  updateEncoderState();
+} // ISR D0-D7
+
+ISR (PCINT0_vect) { // handle pin change interrupt for D8 to D13 here
+  updateEncoderState();
+} // ISR D8-D13
+
 /****************************************************************/
 void setup() {
   uint8_t i, j;
@@ -250,7 +307,7 @@ void setup() {
   unsigned char *ptr = (unsigned char *)&settings;
 
   Serial.begin(115200);
-  Serial.println("My enigma v0.01");
+  Serial.println("My enigma v0.02");
   Serial.println();
 
   // load prev settings
@@ -264,21 +321,22 @@ void setup() {
     settings.preset = 0; // we can have several saved settings
     settings.model = M3;
     settings.ukw = 1;
-    settings.rotor[0] = 0;
-    settings.rotor[1] = 1;
-    settings.rotor[2] = 2;
-    settings.rotor[3] = 3;
-    settings.ringstellung[0] = ' ';
-    settings.ringstellung[1] = 'A';
-    settings.ringstellung[2] = 'A';
-    settings.ringstellung[3] = 'A';
+    settings.walze[0] = 3;
+    settings.walze[1] = 2;
+    settings.walze[2] = 1;
+    settings.walze[3] = 0;
+    settings.ringstellung[0] = 0;
+    settings.ringstellung[1] = 0;
+    settings.ringstellung[2] = 0;
+    settings.ringstellung[3] = 0;
     for (i = 0; i < sizeof(settings.plugboard); i++) {
       settings.plugboard.letter[i]=i;
     }
-    settings.currentrotor[0] = ' ';
-    settings.currentrotor[1] = 'A';
-    settings.currentrotor[2] = 'B';
-    settings.currentrotor[3] = 'C';
+    settings.currentWalze[0] = 0;
+    settings.currentWalze[1] = 0;
+    settings.currentWalze[2] = 0;
+    settings.currentWalze[3] = 27;
+    settings.nextlocation=0;
     settings.odometer = 0;
     //    eeprom_update_block((const void*)&settings, (void*)0, sizeof(settings));
     saveSettings();
@@ -307,22 +365,22 @@ void setup() {
 
   Serial.print(F("reflector: "));
   Serial.println(settings.ukw, DEC);
-  Serial.print(F("Rotors: "));
-  Serial.print(settings.rotor[0], DEC);
+  Serial.print(F("Wheels: "));
+  Serial.print(settings.walze[0], DEC);
   Serial.print(F(" "));
-  Serial.print(settings.rotor[1], DEC);
+  Serial.print(settings.walze[1], DEC);
   Serial.print(F(" "));
-  Serial.print(settings.rotor[2], DEC);
+  Serial.print(settings.walze[2], DEC);
   Serial.print(F(" "));
-  Serial.println(settings.rotor[3], DEC);
+  Serial.println(settings.walze[3], DEC);
   Serial.print(F("ringstellung: "));
-  Serial.print(settings.ringstellung[0]);
+  Serial.print(walzeContent[settings.ringstellung[0]]);
   Serial.print(F(" "));
-  Serial.print(settings.ringstellung[1]);
+  Serial.print(walzeContent[settings.ringstellung[1]]);
   Serial.print(F(" "));
-  Serial.print(settings.ringstellung[2]);
+  Serial.print(walzeContent[settings.ringstellung[2]]);
   Serial.print(F(" "));
-  Serial.println(settings.ringstellung[3]);
+  Serial.println(walzeContent[settings.ringstellung[3]]);
   Serial.println(F("Plugboard (Steckerbrett): "));
   for (i = 0; i < sizeof(settings.plugboard); i++) {
     if (settings.plugboard.letter[i] != i) {
@@ -332,10 +390,19 @@ void setup() {
       Serial.println(settings.plugboard.letter[i],DEC);
     }
   }
+  Serial.print(F("currentWalze: "));
+  Serial.print(walzeContent[settings.currentWalze[0]]);
+  Serial.print(F(" "));
+  Serial.print(walzeContent[settings.currentWalze[1]]);
+  Serial.print(F(" "));
+  Serial.print(walzeContent[settings.currentWalze[2]]);
+  Serial.print(F(" "));
+  Serial.println(walzeContent[settings.currentWalze[3]]);
   Serial.print(F("Odo meter: "));
   Serial.println(settings.odometer, DEC);
 
   Wire.begin(); // enable the wire lib
+  // Setup the port multipler
   i2c_write2(mcp_address,IOCON,0b00011110);   // Init value for IOCON, bank(0)+INTmirror(no)+SQEOP(addr inc)+DISSLW(Slew rate disabled)+HAEN(hw addr always enabled)+ODR(INT open)+INTPOL(act-low)+0(N/A)
   i2c_write2(mcp_address,IODIRA,0xff); // Set all ports to inputs
   i2c_write2(mcp_address,IODIRB,0xff); // Set all ports to inputs
@@ -369,36 +436,18 @@ void setup() {
   HT.sendLed();
   delay(500);
 
-#ifdef TEST
-  // Test the screen
-  Serial.println(F("All LEDs on"));
-  for (i = 0; i <= 128; i++) {
-    HT.setLedNow(i);
-    //    HT.sendLed();
-    delay(500);
-    Serial.print(F("set LED:"));
-    Serial.println(i,DEC);
-  }
-  HT.sendLed();
-  delay(500);
-
-  Serial.println(F("All LEDs OFF"));
-  for (i = 0; i <= 128; i++) {
-    HT.clearLedNow(i);
-    //HT.sendLed();
-    // delay(100);
-    Serial.print(F("clear LED:"));
-    Serial.println(i,DEC);
-  }
-  HT.sendLed();
-
-#endif
-
   // Clear decimal point
   for (i = 10; i <= 13; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, HIGH);
   }
+
+  //Setup encoder wheel interrupt
+  for (i = 0; i < sizeof(encoderPins); i++) {
+    pinMode(encoderPins[i], INPUT_PULLUP);
+    pciSetup(encoderPins[i]);
+  }
+
 } // setup
 
 /****************************************************************/
@@ -461,12 +510,73 @@ uint8_t checkKB() {
 } // checkKB
 
 /****************************************************************/
-void writeLetter(char letter, uint8_t rotor) {
+// Check the wheels, if they moved and something was updated
+// Return true if any wheel changed
+//
+boolean checkWalzes() {
+  uint8_t i;
+  boolean changed;
+
+  changed=false;
+  for (i = 0; i < WALZECNT; i++) {
+    if (encoderMoved[i]) {
+      encoderMoved[i] = false; // ack the change
+      // states when moving in direction A: 11 01 00 10
+      // states when moving in direction B: 11 10 00 01
+      // [prev][curr] state, that means it was moved
+      // 1101 0100 0010 1011 dirA
+      // 1110 1000 0001 0111 dirB
+      switch (encoderState[i] & 0xf) {
+        case B1101: // one quarter, starting to move it
+        case B0100: // two quarter moved
+        case B0010: // three quarter moved
+	  break; // ignore the first 3 clicks
+        case B1011: // next click landed
+	  changed = true; // moved up
+          if (settings.currentWalze[i] == sizeof(walzeContent) - 2) {  // index start at 0 and is terminated with \0
+            settings.currentWalze[i] = 0;
+          } else {
+	    settings.currentWalze[i]++;
+          }
+          break;
+        case B1110: // one quarter, start moving it
+        case B1000: // two quarter moved
+        case B0001: // three quarter moved
+	  break; // ignore the first 3 clicks
+        case B0111: // next click landed
+          changed = true; // moved down
+          if (settings.currentWalze[i] == 0) {
+            settings.currentWalze[i] = sizeof(walzeContent) - 2; // index start at 0 and is terminated with \0
+          } else {
+            settings.currentWalze[i]--;
+          }
+          break;
+#ifdef DEBUG
+      default: // unknown movement, possible started a move and fell back
+          Serial.print(F("unknown: "));
+#endif
+      } // switch
+    } // if moved
+  } // for i in walzecnt
+  return changed;
+} // checkWalzes
+
+/****************************************************************/
+// display something on one of the "wheels"
+// TODO: handle attributes like decimalpoint
+void writeLetter(char letter, uint8_t walze) {
   uint8_t led;
   int8_t i;
   uint16_t val;
 
-  led = (rotor - 1) * 16;
+#ifdef DEBUG
+  Serial.print(F(" Write wheel "));
+  Serial.print(walze,DEC);
+  Serial.print(F(" val "));
+  Serial.println(letter,DEC);
+#endif
+
+  led = (walze - 1) * 16;
   val = fontTable[letter - 32]; // A= 0b1111001111000000 - 0xF3C0
   for (i = 15; i >= 0; i--) {
     if (val & (1 << i)) {
@@ -506,6 +616,10 @@ uint8_t checkPlugboard() {
   uint16_t val,val1,val2;
   letters_t newplugboard;
 
+  //TODO: A way to make the plugboard optional
+  //      probably read the mcp and if it doesn't 
+  //      look right declare plugboard not connected.
+
   // make all io ports inputs-pullup
   i2c_write2(mcp_address,GPPUA,0xff); // enable 100k pullup
   i2c_write2(mcp_address,GPPUB,0xff); //
@@ -537,7 +651,7 @@ uint8_t checkPlugboard() {
     }
 
     //make port "plug" output
-    i2c_write2(mcp,IODIRA+port,0xff ^ (1<<bit)); // Set port to input
+    i2c_write2(mcp,IODIRA+port,0xff ^ (1<<bit));
     
     //set  port "plug" low
     i2c_write2(mcp,GPIOA+port,0xff ^ (1<<bit));
@@ -563,6 +677,7 @@ uint8_t checkPlugboard() {
 	newplugboard.letter[plug]=i;
       }
     }
+
     for (i=0;i<16;i++){
       if ((val2 & (1<<i))==0){
 	if (mcp==mcp_address+1){
@@ -586,12 +701,7 @@ uint8_t checkPlugboard() {
     i2c_write2(mcp,GPIOA+port,0xff);
     i2c_write2(mcp,IODIRA+port,0xff);
     //    i2c_write2(mcp,GPPUA+port,0xff);
-    
-    //read all registers, see if any other port is low
-    //  if yes - update plug table and print out
-    //make port input pullup
   }
-  //PSDEBUG
   // disable all pullups to save power
   i2c_write2(mcp_address,GPPUA,0);
   i2c_write2(mcp_address,GPPUB,0);
@@ -602,6 +712,7 @@ uint8_t checkPlugboard() {
     //    memcpy(settings.plugboard, newplugboard,sizeof(settings.plugboard));
     settings.plugboard=newplugboard;
     saveSettings();
+//PSDEBUG
 #ifndef DEBUG
     Serial.println(F("New Plugboard (Steckerbrett) setting: "));
     for (i = 0; i < sizeof(settings.plugboard); i++) {
@@ -619,6 +730,17 @@ uint8_t checkPlugboard() {
 } // checkPlugboard
 
 /****************************************************************/
+// update the wheels
+ void updateWheels(){
+   writeLetter(walzeContent[settings.currentWalze[0]],0);
+   writeLetter(walzeContent[settings.currentWalze[1]],1);
+   writeLetter(walzeContent[settings.currentWalze[2]],2);
+   writeLetter(walzeContent[settings.currentWalze[3]],3);
+ }  //
+
+
+/****************************************************************/
+/****************************************************************/
 void loop() {
   static uint8_t led = 0;
   static uint8_t ledOn=0;
@@ -629,13 +751,12 @@ void loop() {
 
   pinMode(11, OUTPUT);
   digitalWrite(11, HIGH);
-  checkKB();
 
 //  writeString("HELLO WORLD    ", 400);
 
   writeString(hello, 400);
   delay(500);
-  writeString("V001",0);
+  writeString("V002",0);
   digitalWrite(11,LOW);
   delay(1000);
   digitalWrite(11,HIGH);
@@ -656,7 +777,13 @@ void loop() {
    */
 
 
+  updateWheels();
+
   while (true) {
+    if (checkWalzes()){
+      updateWheels();
+    }
+
     checkPlugboard();
     key=checkKB();
     if (key!=0){
@@ -688,10 +815,7 @@ void loop() {
       }      
     } else { // no key pressed
       if ( cnt >2 ){
-	writeLetter(' ',1);
-	writeLetter(' ',2);
-	writeLetter(' ',3);
-	writeLetter(' ',4);
+	updateWheels();
       } else {
 	cnt++;
       }

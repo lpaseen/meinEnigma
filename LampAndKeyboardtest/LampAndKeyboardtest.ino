d7ca5a676cac2dfa9ad9ddb286a1acb72db74ac7
lpaseen <peters-github@techwiz.ca>
2018-04-02 15:54:28 -0400

    added some comments and improved the output a little

--- LampAndKeyboardtest/LampAndKeyboardtest.ino
+++ LampAndKeyboardtest/LampAndKeyboardtest.ino
@@ -1,5 +1,13 @@
 /****************************************************************
    Simple test of ht16k33 library both led and keyboard
+
+   Usage: Upload this program and then watch the serial console
+   When you press a key the serial console shows what key is
+   pressed and also light up the corresponding LED
+   release the key and the LED goes off.
+
+
+
 */
 
 #include "ht16k33.h"
@@ -14,12 +22,14 @@ HT16K33 HT;
 
 //The order the keys on the keyboard are coded
 //Keyboard scancode table
-//A-Z for keyboard, 0-3 for buttons under walze 0-3
+// "scancodes[]" is an array to map a key to a letter
+// A-Z for keyboard, 0-3 for buttons under walze 0-3
 //(char)pgm_read_byte(&scancodes[0]+key-1)
 //protoboard const char scancodes[] PROGMEM = "OIUZTREWQGHJKLMNBVCXYPASDF0123";
 //Prod board
 const char scancodes[] PROGMEM = "QWERTZUIOASDFPYXCVBNMLGHJK0123";
 
+//"led[]" is an array to map a letter to a led number
 //A=65 so the [0] element contain the number of the LED that shows "A"
 //after 'Z'-65 comes control LEDs [\]^_
 //Usage: led['A'-65] or led[char-65]
@@ -28,21 +38,13 @@ const char scancodes[] PROGMEM = "QWERTZUIOASDFPYXCVBNMLGHJK0123";
 //Prod board
 //                           A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
 const byte led[] PROGMEM = {12, 24, 22, 14,  5, 15, 28, 29, 10, 30, 31, 27, 26, 25, 11, 19,  3,  6, 13,  7,  9, 23,  4, 21, 20,  8};
-//  Q   W   E   R   T   Z   U   I   O
-//  3   4   5   6   7   8   9  10  11
-//
-//    A   S   D   F   G   H   J   K
-//   12  13  14  15  28  29  30  31
-//
-//  P   Y   X   C   V   B   N   M   L
-// 19  20  21  22  23  24  25  26  27
 
 
 /****************************************************************/
 void setup() {
   uint8_t led;
   Serial.begin(38400);
-  Serial.println("Lamp and keyboard test v0.01");
+  Serial.println("Lamp and keyboard test v0.02");
   Serial.println();
   // initialize everything, 0x00 is the i2c address for the first chip (0x70 is added in the class).
   HT.begin(0x00);
@@ -56,47 +58,60 @@ void setup() {
   delay(1000);
   //Next clear them
   Serial.println(F("Clear all LEDs"));
-  for (led = 0; led < 128; led++) {
-    HT.clearLed(led);
-  } // for led
-  HT.sendLed();
+  HT.clearAll();
+  Serial.println();
+  Serial.println(F("Press any key and it will show up below"));
+  Serial.println(F("cnt:    Keyno = Letter,            LED"));
 }
 
 /****************************************************************/
 void loop() {
   static uint8_t prevLed = 0;
-  static uint8_t lastKey = 0, ledOn;
+  static uint8_t lastKey = 0, ledOn, ledOff;
   static int8_t dir = 1;
   int8_t key;
   static int16_t lastKeyCode = 0;
   uint8_t keyCnt;
 
-  key = HT.readKey();
-  if (key != 0) {
-    if (key != lastKey) {
-      if (key < 0) {
+  key = HT.readKey(); // read what key is recently pressed down or released
+  if (key != 0) { // something is changed
+    if (key != lastKey) { //same key as last time ?
+      if (key < 0) { // figure out what letter that is pressed/released
         lastKeyCode = pgm_read_byte(&scancodes[0] + (-key) - 1);
       } else {
         lastKeyCode = pgm_read_byte(&scancodes[0] + key - 1);
       }
-      keyCnt=HT.keysPressed();
-      if (keyCnt<10)
+      keyCnt = HT.keysPressed(); // How many keys are pressed down ?
+      if (keyCnt == 0) // none left, clear the board
+        HT.clearAll();
+
+      if (keyCnt < 10)
         Serial.print(F(" "));
-      Serial.print(keyCnt,DEC);
+      Serial.print(keyCnt, DEC);
       Serial.print(F(": Key # "));
+      if (key >= 0)
+        Serial.print(F(" "));
+      if (key > -10 && key < 10 )
+        Serial.print(F(" "));
       Serial.print(key);
       Serial.print(F(" = "));
       Serial.print((char)lastKeyCode);
 
-      if (key > 0) {
-//        ledOn = pgm_read_byte(led + ((byte)pgm_read_byte(&scancodes[0] + key - 1) - 'A'));
-        ledOn = pgm_read_byte(led + lastKeyCode - 'A');
-        Serial.print(F(", turning on LED: "));
+      if (key > 0) { // if a new key was pressed, turn on cirresponding led
+        ledOn = pgm_read_byte(led + lastKeyCode - 'A'); // get what led to turn on from the "led" array
+        Serial.print(F(", turning on  LED: "));
+        if (ledOn < 10)
+          Serial.print(F(" "));
         Serial.print(ledOn);
         HT.setLedNow(ledOn);
         delay(11);
       } else {
-        HT.clearLedNow(ledOn);
+        ledOff = pgm_read_byte(led + lastKeyCode - 'A');
+        Serial.print(F(", turning off LED: "));
+        if (ledOff < 10)
+          Serial.print(F(" "));
+        Serial.print(ledOff);
+        HT.clearLedNow(ledOff);
       }
       lastKey = key;
       Serial.println();
